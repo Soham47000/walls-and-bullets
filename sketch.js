2aaf7633c03566490609f89e981583646f6b651e
Soham47000 <bdsharmas26@gmail.com>
2021-01-19 17:47:13 +0530

    This is a program to check walls

--- sketch.js
+++ sketch.js
@@ -1,47 +1,27 @@
-var car, wall;
+var wall, thickness;
+var bullet, speed, weight;
 
-var speed,weight;
-
-
-function setup() {
+function setup(){
   createCanvas(1600,400);
-  
-  speed = random(55,90);
-  weight = random(400,1500);
-
-
-  car = createSprite(50,200,50,50);
-  car.velocityX = speed;
-  car.shapeColor = color(255);
-
-
-
-  wall = createSprite(1500,200,30,height/2);
-  wall.shapeColor = "pink";
+ speed = random(55,90);
+ thickness = random(232,321);
+ weight = random(30,52);
+ bullet = createSprite(50,200,50,30);
+ wall=createSprite(1500,200,thickness,height/2);
 }
 
-function draw() {
-  background(0);  
-  if(wall.x - car.x < (car.width + wall.width)/2)
-  {
-    car.velocityX = 0;
-    var deformation = 0.5 * weight * speed * speed/22509;
-    if(deformation>180)
-  {
-    car.shapeColor = color(255,0,0);
+function draw(){
+  background(0,0,0);
+  bullet.velocityX = speed;
+  if(wall.x-bullet.x < (bullet.width+wall.width)/2){
+    bullet.velocityX=0;
+    var damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
+    if(damage>10){
+      wall.shapeColor=color(255,0,0);
+    }
+    if(damage<10){
+      wall.shapeColor=color(0,255,0);
+    }
   }
-
-  if(deformation<180 && deformation>100)
-  {
-  car.shapeColor = color(230,230,0);
-  }
-
-  if(deformation<100)
-  {
-    car.shapeColor = color(0,250,0);
-  }
-
-  }
-  
-  drawSprites();
+    drawSprites();
 }
